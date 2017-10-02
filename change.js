3b65990da07ef067e3d25411e3720e2392338a40
manishsaraan <manish021js@gmail.com>
2017-10-02 14:55:02 +0530

    code coverage implemented

--- change.js
+++ change.js
@@ -1,3 +1,4 @@
+
 var coins = [200, 100, 50, 20, 10, 5, 2, 1]
 function getChange(totalPayable, cashPaid) {
     var change = [];
@@ -15,13 +16,14 @@ function getChange(totalPayable, cashPaid) {
                 remaining = remaining - coin;  // subtract coin from remaining
             }
         }
-    }
-    if(cashPaid == 1337) {
-      ATM = [20, 10, 5, 2];
-      for(var i = 0; i< 18; i++) { ATM.push(100) };
-      return ATM;
-    }
-    else {
+    }  
       return change;  
-    }
+   
 };
+
+/* The code block below ONLY Applies to Node.js - This Demonstrates
+   re-useability of JS code in both Back-end and Front-end! #isomorphic */
+/* istanbul ignore next */
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = getChange;  // allows CommonJS/Node.js require()
+}
